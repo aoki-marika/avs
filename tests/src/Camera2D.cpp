8250d876ef20e978ae95eefab423419f63cfc109
marika <marika@waifu.club>
2019-03-28 16:22:09 -0300

    Fix typo in Camera2D test.

--- tests/src/Camera2D.cpp
+++ tests/src/Camera2D.cpp
@@ -21,7 +21,7 @@ int main()
     camera = new Camera2D(720, 1280);
     expected = Matrix4( 0.002778f,  0.000000f,  0.000000f, -1.000000f,
                         0.000000f, -0.001563f,  0.000000f,  1.000000f,
-                        0.000000f,  0.000000f, -2.000000f,  1.010000f,
+                        0.000000f,  0.000000f, -2.000000f,  1.000000f,
                         0.000000f,  0.000000f,  0.000000f,  1.000000);
     Assert::Matrix4Equals("PV matrix 720x1280", camera->GetMatrix(), expected);
     delete camera;
