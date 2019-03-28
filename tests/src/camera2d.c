9ca74bd36574d5f9549218a6425e8f79abdd0a2f
marika <marika@waifu.club>
2019-03-28 15:57:03 -0300

    Update camera2d.c.

--- tests/src/camera2d.c
+++ tests/src/camera2d.c
@@ -1,29 +1,29 @@
 #include <stdlib.h>
 #include <avs/Camera2D.hpp>
 
-#include "utils.h"
+#include "Assert.hpp"
 
 int main()
 {
     Camera2D *camera;
-    Matrix4 correct;
+    Matrix4 expected;
 
     // pv matrix 1280x720
     camera = new Camera2D(1280, 720);
-    correct = Matrix4( 0.001563f,  0.000000f,  0.000000f, -1.000000f,
-                       0.000000f, -0.002778f,  0.000000f,  1.000000f,
-                       0.000000f,  0.000000f, -2.000000f,  1.000000f,
-                       0.000000f,  0.000000f,  0.000000f,  1.000000f);
-    assert_matrix4_equals("pv matrix 1280x720", camera->GetMatrix(), correct);
+    expected = Matrix4( 0.001563f,  0.000000f,  0.000000f, -1.000000f,
+                        0.000000f, -0.002778f,  0.000000f,  1.000000f,
+                        0.000000f,  0.000000f, -2.000000f,  1.000000f,
+                        0.000000f,  0.000000f,  0.000000f,  1.000000f);
+    Assert::Matrix4Equals("PV matrix 1280x720", camera->GetMatrix(), expected);
     delete camera;
 
     // pv matrix 720x1280
     camera = new Camera2D(720, 1280);
-    correct = Matrix4( 0.002778f,  0.000000f,  0.000000f, -1.000000f,
-                       0.000000f, -0.001563f,  0.000000f,  1.000000f,
-                       0.000000f,  0.000000f, -2.000000f,  1.000000f,
-                       0.000000f,  0.000000f,  0.000000f,  1.000000);
-    assert_matrix4_equals("pv matrix 720x1280", camera->GetMatrix(), correct);
+    expected = Matrix4( 0.002778f,  0.000000f,  0.000000f, -1.000000f,
+                        0.000000f, -0.001563f,  0.000000f,  1.000000f,
+                        0.000000f,  0.000000f, -2.000000f,  1.010000f,
+                        0.000000f,  0.000000f,  0.000000f,  1.000000);
+    Assert::Matrix4Equals("PV matrix 720x1280", camera->GetMatrix(), expected);
     delete camera;
 
     return EXIT_SUCCESS;
