fae8ed36f7bc6b490613de0bc5324b8da47d2d5b
marika <marika@waifu.club>
2019-04-21 19:23:09 -0300

    Add VertexBuffer::SetQuad().

--- src/VertexBuffer.cpp
+++ src/VertexBuffer.cpp
@@ -11,3 +11,18 @@ void VertexBuffer::DrawAll()
 {
     glDrawArrays(GL_TRIANGLES, 0, num_vertices);
 }
+
+void VertexBuffer::SetQuad(unsigned int index,
+                           Vector3 a,
+                           Vector3 b,
+                           Vector3 c,
+                           Vector3 d)
+{
+    const std::array<Vector3, num_quad_vertices> vertices =
+    {
+        a, b, d,
+        b, c, d,
+    };
+
+    this->SetVertices(index, &vertices);
+}
