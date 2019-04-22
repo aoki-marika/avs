102754f9a3794fa0a0fa3f4404b8506e173cbd72
marika <marika@waifu.club>
2019-04-21 21:37:27 -0300

    Fix Camera2D not showing vertices at Z 0.

--- include/avs/Camera2D.hpp
+++ include/avs/Camera2D.hpp
@@ -7,6 +7,6 @@ class Camera2D : public Camera
     public:
         // create a 2d camera with the given resolution in pixels
         // vertices drawn with this camera have their units in pixels and a top left origin
-        // +x = left, -x = right, +y = down, -y = up
+        // +x = right, -x = left, +y = down, -y = up
         Camera2D(float width, float height);
 };
