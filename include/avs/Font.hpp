26a17781f4fd7cebb87e416f89751319534ab50c
marika <marika@waifu.club>
2019-04-23 17:15:22 -0300

    Update Font to return Glyphs that contain the image name, bearing, and advance.

--- include/avs/Font.hpp
+++ include/avs/Font.hpp
@@ -1,17 +1,24 @@
 #pragma once
 
+#include "Vector2.hpp"
 #include "Atlas.hpp"
 
 class Font
 {
     public:
+        struct Glyph
+        {
+            // the name of the image of this glyph in its fonts atlas
+            std::string Image;
+
+            // the bearing and advance of this glyph, in pixels
+            Vector2 Bearing, Advance;
+        };
+
         // get the atlas containing this fonts characters
         virtual Atlas *GetAtlas() = 0;
 
-        // get the image name for the given character in this fonts atlas
-        // returns nullptr if the given character is not in this font
-        virtual std::string *GetImage(char32_t character) = 0;
-
-        // get the spacing for after the given character in this font
-        virtual float GetSpacing(char32_t character) = 0;
+        // get the glyph in this font for the given character
+        // returns nullptr if this font has no glyph for the given character
+        virtual Glyph *GetGlyph(char32_t character) = 0;
 };
